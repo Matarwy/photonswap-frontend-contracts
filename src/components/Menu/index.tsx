3182d9a12e1f402a5dcc5c5f72af7b54ce6b8c5e
PhotonSwap <photonswap@gmail.com>
2021-06-27 17:31:24 +0100

    x

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react'
+import { useWallet } from '@binance-chain/bsc-use-wallet'
+import { allLanguages } from 'config/localisation/languageCodes'
+import { LanguageContext } from 'contexts/Localisation/languageContext'
+import useTheme from 'hooks/useTheme'
+import { usePriceCakeBusd } from 'state/hooks'
+import { Menu as UikitMenu } from 'uikit'
+import config from './config'
+
+const Menu = (props) => {
+  const { account, connect, reset } = useWallet()
+  const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
+  const { isDark, toggleTheme } = useTheme()
+  const cakePriceUsd = usePriceCakeBusd()
+
+  return (
+    <UikitMenu
+      account={account}
+      login={connect}
+      logout={reset}
+      isDark={isDark}
+      toggleTheme={toggleTheme}
+      currentLang={selectedLanguage && selectedLanguage.code}
+      langs={allLanguages}
+      setLang={setSelectedLanguage}
+      cakePriceUsd={cakePriceUsd.toNumber()}
+      links={config}
+      priceLink="https://bscscan.com/address/0x10a96eaecdf605a5e682bfcd2278ca7cc682eb84"
+      {...props}
+    />
+  )
+}
+
+export default Menu
